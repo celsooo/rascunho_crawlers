fc8b36c95085b6a60d27080b54c0d39c26d5cd99
Celso Junior <celso.bergmann@gmail.com>
2019-08-15 21:49:12 -0300

    incluido loop entre paginas

--- trucadao.py
+++ trucadao.py
@@ -2,54 +2,43 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup as soup
 from openpyxl import Workbook
-my_url = "https://www.trucadao.com.br/venda/caminhoes-usados"
-uClient = urlopen(my_url)
-page_html = uClient.read()
-uClient.close()
-
-#html parsing
-page_soup = soup(page_html, "html.parser")
-
-#Data extraction
-preco = page_soup.find_all("h4",{"class":"col-sm-4 price"})
-titulo = page_soup.find_all("h2",{"class":"col-sm-8"})
-tipo = page_soup.find_all("span", {"itemprop":"audience"})
-marca = page_soup.find_all("span", {"itemprop":"name"})
-modelo = page_soup.find_all("span", {"itemprop":"model"})
-local = page_soup.find_all("span", {"itemprop":"addressLocality"})
-
 
 #Using OpenPyxl
 book = Workbook()
 sheet = book.active
 
-#Iterate through data and write into worksheet
-for row in range(40):
-    rows = (
-        (preco[row].text, titulo[row].text, tipo[row].text, marca[row].text, modelo[row].text, local[row].text)
-    )
-    sheet.append(rows)
-    row = row +1
+
+for page in range(1, 3):
+    my_url = "https://www.trucadao.com.br/venda/caminhoes-usados?page=" + str(page)
+    print(my_url)
+    uClient = urlopen(my_url)
+    page_html = uClient.read()
+    uClient.close()
+    #html parsing
+    page_soup = soup(page_html, "html.parser")
+    #Data extraction
+    preco = page_soup.find_all("h4",{"class":"col-sm-4 price"})
+    titulo = page_soup.find_all("h2",{"class":"col-sm-8"})
+    tipo = page_soup.find_all("span", {"itemprop":"audience"})
+    marca = page_soup.find_all("span", {"itemprop":"name"})
+    modelo = page_soup.find_all("span", {"itemprop":"model"})
+    local = page_soup.find_all("span", {"itemprop":"addressLocality"})
+    page = page + 1
+    my_url = ("https://www.trucadao.com.br/venda/caminhoes-usados?page=" + str(page))
+
+    for row in range(40):
+        rows = (
+            (preco[row].text, titulo[row].text, tipo[row].text, marca[row].text, modelo[row].text, local[row].text)
+        )
+        sheet.append(rows)
+        row = row + 1
+
 
 book.save("trucadao.xlsx")
 
 
 
-
-
-
-
-
-
 #Extração do link - REFINAR
 #link2 = page_soup.find_all('a', {"class":"link-full-ad col-xs-12"})
-
 '''
-for a in range(40):
-    print(titulo[a].text)
-    print(preco[a].text)
-    print(tipo[a].text)
-    print(marca[a].text)
-    print(modelo[a].text)
-    print(local[a].text)
-    '''
+
